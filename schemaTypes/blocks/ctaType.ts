21da4497df17f4bab88cce6664556982e3fdcfda
Stuart Sewell <stuart@preoptima.com>
2025-04-07 15:03:40 +0100

    feat: refactor ctaType to use object type and define fields for link

--- schemaTypes/blocks/ctaType.ts
+++ schemaTypes/blocks/ctaType.ts
@@ -1,10 +1,28 @@
-import {defineType} from 'sanity'
+import {defineField, defineType} from 'sanity'
 
 export const ctaType = defineType({
   name: 'cta',
   title: 'CTA',
-  type: 'link',
-  options: {
-    enableText: true,
+  type: 'object',
+  fields: [
+    defineField({
+      name: 'link',
+      title: 'CTA',
+      type: 'link',
+      options: {
+        enableText: true,
+      },
+    }),
+  ],
+  preview: {
+    select: {
+      title: 'link.text',
+    },
+    prepare(selection) {
+      return {
+        title: selection.title,
+        subtitle: 'CTA',
+      }
+    },
   },
 })
