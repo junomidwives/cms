d7c70e80bb15c3f05db22c8395fd67e14f79b8b2
Stuart Sewell <stuart@preoptima.com>
2025-04-10 18:22:20 +0100

    chore: update dependencies and configure Sanity CLI
    - Added @sanity/cli dependency version 3.83.0 to package.json
    - Updated sanity.cli.ts to include a trailing comma in the dataset configuration and enabled auto-updates for studios

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -3,11 +3,7 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 's88cxh4b',
-    dataset: 'production'
+    dataset: 'production',
   },
-  /**
-   * Enable auto-updates for studios.
-   * Learn more at https://www.sanity.io/docs/cli#auto-updates
-   */
   autoUpdates: true,
 })
