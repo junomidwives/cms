f663ec34847ab2cc78abba3ad31f68c3b8aedb43
Stuart Sewell <stuart@preoptima.com>
2025-04-29 10:17:08 +0100

    feat: enhance Stackbit configuration with sitemap generation and model extensions

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,6 +1,5 @@
-import {defineStackbitConfig} from '@stackbit/types'
+import {defineStackbitConfig, SiteMapEntry} from '@stackbit/types'
 import {SanityContentSource} from '@stackbit/cms-sanity'
-import path from 'path'
 
 export default defineStackbitConfig({
   stackbitVersion: '~0.6.0',
@@ -9,20 +8,43 @@ export default defineStackbitConfig({
   contentSources: [
     new SanityContentSource({
       rootPath: __dirname,
-      studioPath: path.join(__dirname, 'studio'),
+      studioPath: __dirname,
       studioUrl: 'https://juno-midwives.sanity.studio',
       projectId: process.env.SANITY_PROJECT_ID!,
       token: process.env.SANITY_ACCESS_TOKEN!,
       dataset: process.env.SANITY_DATASET || 'production',
     }),
   ],
-  // @ts-expect-error
-  mapModels: ({models}) => {
-    return models.map((model) => {
-      if (model.name === 'page') {
-        return {...model, type: 'page', urlPath: '/{slug}'}
-      }
-      return model
-    })
+  modelExtensions: [
+    {name: 'Page', type: 'page', urlPath: '/{slug}'},
+    {name: 'Blog', type: 'page', urlPath: '/blog/{slug}'},
+    {name: 'BirthStory', type: 'page', urlPath: '/birth-stories/{slug}'},
+  ],
+  sitemap: ({documents, models}) => {
+    const pageModels = models.filter((model) => model.type === 'page')
+
+    return documents
+      .filter((d) => pageModels.some((m) => m.name === d.modelName))
+      .map((document) => {
+        // Map the model name to its corresponding URL
+        const urlModel = (() => {
+          switch (document.modelName) {
+            case 'Page':
+              return 'otherPage'
+            case 'Blog':
+              return 'otherBlog'
+            default:
+              return null
+          }
+        })()
+
+        return {
+          stableId: document.id,
+          urlPath: `/${urlModel}/${document.id}`,
+          document,
+          isHomePage: false,
+        }
+      })
+      .filter(Boolean) as SiteMapEntry[]
   },
 })
