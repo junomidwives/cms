01eaa29c43aee26bd32c9cbe35105d09a9dbed4e
Stuart Sewell <stuart@preoptima.com>
2025-09-22 16:50:49 +0100

    refactor: remove Logo component and update Sanity configuration to use Baby icon

--- components/Logo.tsx
+++
@@ -1,9 +0,0 @@
-export function Logo() {
-  return (
-    <img
-      style={{width: '25px', height: '25px'}}
-      src="../static/Logo.png"
-      alt="Juno Midwives logo"
-    />
-  )
-}
--- sanity.config.ts
+++ sanity.config.ts
@@ -7,7 +7,7 @@ import {table} from '@sanity/table'
 import {linkField} from 'sanity-plugin-link-field'
 import {dashboardTool} from '@sanity/dashboard'
 import {netlifyWidget} from 'sanity-plugin-dashboard-widget-netlify'
-import {Logo} from './components/Logo'
+import {Baby} from 'lucide-react'
 
 const visionDev = process.env.NODE_ENV === 'development' ? [visionTool()] : []
 
@@ -16,7 +16,7 @@ export default defineConfig({
   title: 'Juno Midwives',
   projectId: 's88cxh4b',
   dataset: 'production',
-  icon: Logo,
+  icon: Baby,
 
   plugins: [
     structureTool({
